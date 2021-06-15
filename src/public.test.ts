b7a6470c32116a95c74c8b9488cb1efba962b812
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2021-06-15 20:21:25 +0700

    Add getConnectionCount unit test

--- src/public.test.ts
+++ src/public.test.ts
@@ -21,3 +21,18 @@ describe("getNodeInfo", () => {
         await expect(snarkos.getNodeInfo).rejects.toThrowError();
     });
 });
+
+describe("getConnectionCount", () => {
+    test("with accessible node url", async () => {
+        const snarkos = new Client(SNARKOS_NODE_URL);
+        const count = await snarkos.getConnectionCount();
+
+        expect(count).toBeGreaterThan(0);
+    });
+
+    test("with inaccessible node url", async () => {
+        expect.assertions(1);
+        const snarkos = new Client("http://localhost:9000");
+        await expect(snarkos.getConnectionCount).rejects.toThrowError();
+    });
+});
