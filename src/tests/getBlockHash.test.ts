5d64ed6cfa85b8095a2b8363fdb26e17158ceeb6
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2021-06-16 14:36:03 +0700

    Use dynamic block height to test getBlockHash

--- src/tests/getBlockHash.test.ts
+++ src/tests/getBlockHash.test.ts
@@ -5,8 +5,10 @@ const SNARKOS_NODE_URL = "http://localhost:3030";
 describe("getBlockHash", () => {
     test("with accessible node url", async () => {
         const snarkos = new Client(SNARKOS_NODE_URL);
-        const blockHash = await snarkos.getBlockHash(1000);
-
+        // Get blocktemplate first
+        const blockTemplate = await snarkos.getBlockTemplate();
+        const blockHeight = blockTemplate.blockHeight - 1;
+        const blockHash = await snarkos.getBlockHash(blockHeight);
         expect(blockHash).toBeDefined();
     });
 
